9c107a162c2edfb0309fa512c674152cf62ab814
Gytis Karčiauskas <gytis@karciauskas.lt>
2012-01-20 00:54:20 +0200

    added main paths

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Absolute framework path
+ */
+define('ROOT_PATH', dirname(__FILE__));
+
+/**
+ * Absolute core path
+ */
+define('CORE_PATH', ROOT_PATH . '/core');
+
+/**
+ * Absolute modules path
+ */
+define('MODULES_PATH', ROOT_PATH . '/modules');
+
+/**
+ * Absolute layouts path
+ */
+define('LAYOUTS_PATH', ROOT_PATH . '/layouts');
---
+++ public_html/index.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * gFrame - simple PHP framework
+ *
+ * @author Gytis Karčiauskas <gytis@karciauskas.lt>
+ * @copyright Copyright (c) 2011-2012
+ */
+
+require('../config.php');
