543b8531f1bbecffadd341d7d24d850fe224821b
Gytis Karčiauskas <gytis@karciauskas.lt>
2012-01-20 20:38:06 +0200

    added default layout and view

---
+++ layouts/index.layout.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    
+    <body>
+        <h1>It works!</h1>
+        <?php $this->get_view(); ?>
+    </body>
+</html>
---
+++ modules/index/index.view.index.php
@@ -0,0 +1,1 @@
+<p>Default module INDEX</p>
