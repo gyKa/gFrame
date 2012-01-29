853d4b4a1056f39510aa06aabbf03916148f0a76
Gytis Karčiauskas <gytis@karciauskas.lt>
2012-01-29 02:07:06 +0200

    added modules and layouts dirs. Added exception for admin module

--- config.php
+++ config.php
@@ -17,6 +17,9 @@ $config['DEFAULT_USER_MODULE'] = ''; // Default module for authorized users. If 
  * DIRECTORIES
  */
 $config['CSS_DIR'] = 'css';
+$config['MODULES_DIR'] = 'modules';
+$config['LAYOUTS_DIR'] = 'layouts';
+
 
 /*
  * URLS
@@ -29,7 +32,7 @@ $config['CSS_URL'] = $config['BASE_URL'] . $config['CSS_DIR']; // Absolute CSS u
  */
 $config['ROOT_PATH'] = dirname(__FILE__); // Absolute framework path
 $config['CORE_PATH'] = $config['ROOT_PATH'] . '/core'; // Absolute core path
-$config['MODULES_PATH'] = $config['ROOT_PATH'] . '/modules'; // Absolute modules path
-$config['LAYOUTS_PATH'] = $config['ROOT_PATH'] . '/layouts'; // Absolute layouts path
+$config['MODULES_PATH'] = $config['ROOT_PATH'] . '/' . $config['MODULES_DIR']; // Absolute modules path
+$config['LAYOUTS_PATH'] = $config['ROOT_PATH'] . '/' . $config['LAYOUTS_DIR']; // Absolute layouts path
 $config['CSS_PATH'] = $config['ROOT_PATH'] . '/' . $config['CSS_DIR']; // Absolute CSS path
 $config['PUBLIC_PATH'] = dirname(__FILE__); // Public path
--- init.php
+++ init.php
@@ -20,13 +20,23 @@ require(CORE_PATH . '/System.class.php');
 require(CORE_PATH . '/Controller.class.php');
 
 System::start();
-System::set_debug_mode(DEBUGGING_MODE);
-System::set_module_path(MODULES_PATH);
 
 $default_module = !empty($_SESSION['user']) ? DEFAULT_USER_MODULE : DEFAULT_GUEST_MODULE;
 
 $module = !empty($_GET['module']) ? $_GET['module'] : $default_module;
 $action = !empty($_GET['action']) ? $_GET['action'] : NULL;
+
+$modules_path = MODULES_PATH;
+$layouts_path = LAYOUTS_PATH;
+
+if ($module === 'admin')
+{
+    $modules_path = ROOT_PATH . '/' . MODULES_DIR;
+    $layouts_path = ROOT_PATH . '/' . LAYOUTS_DIR;
+}
+
+System::set_debug_mode(DEBUGGING_MODE);
+System::set_module_path($modules_path);
 
 System::include_module_item($module, 'model');
 
@@ -34,9 +44,9 @@ if (System::include_module_item($module, 'controller') === FALSE)
     System::redirect($default_module);
 
 $Module = new Module();
-$Module->set_layouts_path(LAYOUTS_PATH);
+$Module->set_layouts_path($layouts_path);
 $Module->set_module_name($module);
-$Module->set_modules_path(MODULES_PATH);
+$Module->set_modules_path($modules_path);
 
 if (!is_null($action) && method_exists($Module, $action))
     $Module->set_action($action);
