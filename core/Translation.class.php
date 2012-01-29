05eaacdff554e868b48cc27717bdda7ca0411e7e
Gytis Karčiauskas <gytis@karciauskas.lt>
2012-01-29 12:10:34 +0200

    added some static methods

--- core/Translation.class.php
+++ core/Translation.class.php
@@ -3,7 +3,17 @@
 require_once(dirname(__FILE__) . '/../libs/yaml/lib/sfYamlParser.php');
 require_once(dirname(__FILE__) . '/../libs/yaml/lib/sfYamlDumper.php');
 
-class Translate
+class Translation
 {
-
+    private static $translations_file;
+    
+    public static function set_translation_file($translations_file) {
+        self::$translations_file = $translations_file;
+    }
+    
+    public static function translate($text)
+    {
+        echo $text;
+    }
+    
 }
