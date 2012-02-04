9b8d78c95b752d4b11d91e28821865efc93bf956
Gytis Karčiauskas <gytis@karciauskas.lt>
2012-02-05 00:14:49 +0200

    applied Form class for admin's login screen

--- layouts/admin.layout.php
+++ layouts/admin.layout.php
@@ -7,13 +7,13 @@
         <link rel="stylesheet" type="text/css" href="<?php echo CSS_URL; ?>/960.css" />
         <link rel="stylesheet" type="text/css" href="<?php echo CSS_URL; ?>/common.css" />
         <link rel="stylesheet" type="text/css" href="<?php echo CSS_URL; ?>/text.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo CSS_URL; ?>/admin.css" />
         
         <base href="<?php echo BASE_URL; ?>">
     </head>
     
     <body>
         <div class="container_12">
-            <h1>Administration</h1>
             <?php $this->get_view(); ?>
         </div>
     </body>
--- modules/admin/admin.view.index.php
+++ modules/admin/admin.view.index.php
@@ -1,1 +1,23 @@
-<p>Admin module</p>
+<div class="grid_4 suffix_4 prefix_4">
+    <div id="login_form">
+        <div id="login_form_title" class="text_center">gFrame</div>
+    
+        <div id="login_form_body">
+            <?php
+                $form = new Form();
+                
+                $form->set_method('post')
+                        ->set_action('index.php?lang=en&method=admin&action=check_login')
+                        ->add_element('text',
+                                array('label' => array('title' => 'Username'),
+                                    'name' => 'username'))
+                        ->add_element('password', 
+                                array('name' => 'password',
+                                    'label' => array('title' => 'Password')))
+                        ->add_element('submit', 
+                                array('value' => 'Login'))
+                        ->print_output();
+            ?>
+        </div>
+    </div>
+</div>
